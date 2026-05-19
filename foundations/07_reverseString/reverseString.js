4a46744d9499cd7276dc6125bc0405b48b6504e5
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 15:01:11 +0300

    Write a function that reverses a string
    This function uses array methods to reverse a string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+// A function which takes a string and reverses it
+const reverseString = function(str) {
+  // Return splitted, reversed and joined again string
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
