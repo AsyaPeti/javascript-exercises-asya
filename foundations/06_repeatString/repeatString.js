02aa274635746b1d5cbb4e7a7a2bb405dca9fef1
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 15:10:44 +0300

    Correct the typo

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,9 +1,9 @@
-// A function takes a string and a number and repeat the string
+// A function takes a string and a number and repeats the string
 const repeatString = function(string, num) {
   let longString = "";
   for (i = 1; i <= num; i++) {longString += string;}
   
-  // If a number is less than 0 return "ERROR"
+  // If the number is less than 0 return "ERROR"
   if (num < 0) {
     return "ERROR";
   } else {
