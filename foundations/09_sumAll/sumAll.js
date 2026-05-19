aa2888b54fa072eaa74059c6e0fcb2991fcc1c31
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 18:18:49 +0300

    Optimize the if-conditionals for number checking
    Previously, the numbers were checked using comparisons; replaced them
    with methods. Also shortened the conditional search for the larger
    number to avoid repeating the formula.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,16 +1,15 @@
 // This function summarizes all the integers within a given range
 const sumAll = function(num1, num2) {
-  // Determine if the given numbers are positive integers and find the larger
-  if (!(typeof(num1) === "number" && typeof(num2) === "number" &&
-       num1 % 1 === 0 && num2 % 1 === 0 && num1 > 0 && num2 > 0)) {
+  if (!(Number.isInteger(num1) && Number.isInteger(num2) &&
+        num1 >= 0 && num2 >= 0)) {
     return "ERROR";
-  } else if (num2 >= num1) {
-    let count = num2 - num1 + 1;
-    return count * (num1 + num2) / 2;
-  } else {
-    let count = num1 - num2 + 1;
-    return count * (num1 + num2) / 2;
-  }
+  } else if (num2 < num1) {
+    [num1, num2] = [num2, num1];
+  } 
+  
+  // Arithmetic Series Formula
+  let count = num2 - num1 + 1;
+  return count * (num1 + num2) / 2;
 };
 
 // Do not edit below this line
