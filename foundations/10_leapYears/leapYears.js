b6ccb2045b947e0fb5bd19854be398aca8672ae7
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 19:04:17 +0300

    Shorten the comments

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,24 +1,17 @@
 // A function which checks whether a given year is a leap year
 const leapYears = function(year) {
-  // If the year is a century year
+  // Checks for century years
   if (year % 100 === 0) {
-    // If the year divs 400 without a remainder
     if (year % 400 === 0) {
-      // Return true
       return true;
-    // Else ...
     } else {
-      // Return false
       return false;
     }
   
-  // Else if the year divs 4 without a remainder
+  // Checks for non-century years
   } else if (year % 4 === 0) {
-    // Return true
     return true;
-  // Else ...
   } else {
-    // Return false
     return false;
   }
 };
