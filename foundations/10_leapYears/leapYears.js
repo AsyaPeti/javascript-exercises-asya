d8f4f015cc1045bd458e4d39ab292b321a035e5b
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 19:10:55 +0300

    Shorten the if-conditional
    Previously, the nested if-conditional was used. For brevity, combined
    all conditions into one line.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,15 +1,6 @@
 // This function determines if a given year is a leap year
 const leapYears = function(year) {
-  // Checks for century years
-  if (year % 100 === 0) {
-    if (year % 400 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  
-  // Checks for non-century years
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (!(year % 100 === 0) || (year % 400 === 0))) {
     return true;
   } else {
     return false;
