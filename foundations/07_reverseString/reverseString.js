dad944e132269c0ef5fb0117d59c9892dc51a3eb
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 15:15:48 +0300

    Rewrite a comment

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,4 @@
-// A function which takes a string and reverses it
+// A function that reverses a string
 const reverseString = function(str) {return str.split("").reverse().join("");};
 // Do not edit below this line
 module.exports = reverseString;
