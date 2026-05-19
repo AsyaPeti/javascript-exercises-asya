f04fb68cbd31cc29643f03ecea681c18a73aa335
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 18:52:43 +0300

    Create a function that identifies leap years
    The function checks if the given year is a leap year and returns true or
    false accordingly.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
-
+// A function which checks whether a given year is a leap year
+const leapYears = function(year) {
+  // If the year is a century year
+  if (year % 100 === 0) {
+    // If the year divs 400 without a remainder
+    if (year % 400 === 0) {
+      // Return true
+      return true;
+    // Else ...
+    } else {
+      // Return false
+      return false;
+    }
+  
+  // Else if the year divs 4 without a remainder
+  } else if (year % 4 === 0) {
+    // Return true
+    return true;
+  // Else ...
+  } else {
+    // Return false
+    return false;
+  }
 };
 
 // Do not edit below this line
