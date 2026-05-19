bec54e7bbee18a593846845b9cb3b14f39c6454a
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 15:51:36 +0300

    Write a function that clears the array
    This function removes an arbitrary number of given arguments from a
    given array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+// A function takes an array and some more arguments and returns an array
+const removeFromArray = function(arr, ...excArgs) {
+  // A new array is equal to a filtered given array
+  let cleanArr = arr.filter((item) => !excArgs.includes(item));
+  // Return the new array
+  return cleanArr;
 };
 
 // Do not edit below this line
