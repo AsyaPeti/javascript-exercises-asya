7266414324197bc4deefe122272f6856b513e4d7
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-19 18:06:49 +0300

    Rewrite the function using the Arithmetic Series
    The function used a loop. Now, it uses the arithmetic series formula to
    calculate the same thing, which is significantly faster.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,17 +1,15 @@
 // This function summarizes all the integers within a given range
 const sumAll = function(num1, num2) {
-  let sum = 0;
-  
   // Determine if the given numbers are positive integers and find the larger
   if (!(typeof(num1) === "number" && typeof(num2) === "number" &&
        num1 % 1 === 0 && num2 % 1 === 0 && num1 > 0 && num2 > 0)) {
     return "ERROR";
   } else if (num2 >= num1) {
-    for (let i = num1; i <= num2; i++) {sum += i;}
-    return sum;
+    let count = num2 - num1 + 1;
+    return count * (num1 + num2) / 2;
   } else {
-    for (let i = num2; i <= num1; i++) {sum += i;}
-    return sum;
+    let count = num1 - num2 + 1;
+    return count * (num1 + num2) / 2;
   }
 };
 
