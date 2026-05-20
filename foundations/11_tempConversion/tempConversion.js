fb9ff139f5b8e154c26b14498ad147a1ef1fe994
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-05-20 14:03:23 +0300

    Create two functions for temperature conversion
    One function converts from Fahrenheit to Celsius, and the other converts
    from Celsius to Fahrenheit.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+// A function converts given Fahrenheit degrees to Celsius degrees
+const convertToCelsius = function(fahrDegrees) {
+  // Return Celsius degrees rounded to one decimal place
+  return Math.round(5 / 9 * (fahrDegrees - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// A function converts given Celsius degrees to Fahrenheit degrees
+const convertToFahrenheit = function(celsDegrees) {
+  // Return Fahrenheit degrees rounded to one decimal place
+  return Math.round((9 / 5 * celsDegrees + 32) * 10) / 10;
 };
 
 // Do not edit below this line
