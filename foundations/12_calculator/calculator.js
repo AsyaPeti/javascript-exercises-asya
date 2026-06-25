e35972845ee107176795a0955e73710403933df8
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-25 20:50:12 +0300

    Pass all factorial function tests

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -25,12 +25,8 @@ const power = function(base, exp) {
 
 // This function calculates the factorial of a given number
 const factorial = function(num) {
-  // The result is 1
   let result = 1;
-  // Loop from 1 to num, including num
-    // Multiply the result by an iterator
   for (let i = 1; i <= num; i++) result *= i;
-  // Return the result
   return result;
 };
 
