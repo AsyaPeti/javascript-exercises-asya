fcaa05bfc10a5175bcef73345fa984fc0e1e82ba
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-25 20:29:23 +0300

    Write a function that computes the factorial

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -23,8 +23,15 @@ const power = function(base, exp) {
   return base ** exp;
 };
 
-const factorial = function() {
-	
+// This function calculates the factorial of a given number
+const factorial = function(num) {
+  // The result is 1
+  let result = 1;
+  // Loop from 1 to num, including num
+    // Multiply the result by an iterator
+  for (let i = 1; i <= num; i++) result *= i;
+  // Return the result
+  return result;
 };
 
 // Do not edit below this line
