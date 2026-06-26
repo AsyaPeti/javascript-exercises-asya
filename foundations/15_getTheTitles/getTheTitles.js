06cb102a26e1dd6cfffd6e114d6e5307b1ead3c8
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-26 15:49:40 +0300

    Clean up the comments

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,5 @@
 // This function extracts the titles from book objects in an array
 const getTheTitles = function(books) {
-  // Replace the objects with their "title" properties in the array
   return books.map((book) => book.title);
 };
 
