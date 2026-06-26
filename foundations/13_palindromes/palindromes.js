ab925d80b97876c6566e8cedfd9ddd1078d0b78b
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-26 14:21:21 +0300

    Pass all palindromes tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,25 +1,22 @@
 // This function determines if a given string is a palindrome
 const palindromes = function(string) {
-  // A new filtered array is the string
+  
+  // A new filtered array made from the string
   const filteredArr = string
-    // Split into symbols
     .split("")
-    // Filtered out non-letters and non-digits
     .filter((symbol) => /^[a-zA-Z0-9]+$/.test(symbol));
-  // A new forward string is the array
+  
+  // A new forward string made from the array
   let forwardStr = filteredArr
-    // Joined into a string
     .join("")
-    // Changed to lowercase
     .toLowerCase();
-  // A new reverse string is the array
+  
+  // A new reverse string made from the array
   let reverseStr = filteredArr
-    // Reversed
     .reverse()
-    // Joined into a string
     .join("")
-    // Changed to lowercase
     .toLowerCase();
+  
   // Compare the forward and reverse strings and return the result
   return forwardStr === reverseStr;
 };
