ccdbc7d526f558b33743403a59dc10620796431a
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-26 15:46:07 +0300

    Write a method to extract properties from objects

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+// This function extracts the titles from book objects in an array
+const getTheTitles = function(books) {
+  // Replace the objects with their "title" properties in the array
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
