40d3e3e5609be15fd23ec2e8b6f658d3477089fd
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-02 16:11:25 +0300

    Pass all the tests for the findTheOldest function

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,19 +1,17 @@
 // This function identifies the oldest person in an array of people
 const findTheOldest = function(people) {
-  // The oldest person in the array
   let result = people.reduce((oldest, person) => {
-    // If a date of death is not provided, it is set to the current year
     if (!oldest.yearOfDeath) oldest.yearOfDeath = new Date().getFullYear();
     if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
-    // If the current person is older than the oldest person
+    
     if ((person.yearOfDeath - person.yearOfBirth) > 
         (oldest.yearOfDeath - oldest.yearOfBirth)) {
-      // The current one is the oldest
       oldest = person;
     }
-    // Return the oldest
+    
     return oldest;
   });
+  
   return result;
 };
 
